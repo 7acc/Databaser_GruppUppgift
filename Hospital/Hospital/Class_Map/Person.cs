06891779cb21dba572b9187e2cdd1bb296439d1d
lundgren84 <lundgren84@hotmail.se>
2016-04-22 15:28:26 +0200

    Adding vs file Hospital

---
+++ Hospital/Hospital/Class_Map/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hospital
+{
+    public abstract class Person
+    {       
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string  Adress { get; set; }
+        public string  PhoneNR { get; set; }
+
+    }
+}

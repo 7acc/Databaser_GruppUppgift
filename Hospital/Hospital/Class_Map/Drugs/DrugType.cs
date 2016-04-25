1e7f8d3ead3e9ae4dd39f9d59c6f14f455373d4c
7acc <jacc321@gmail.com>
2016-04-25 12:46:42 +0200

    Added all the Classes // Jacc

---
+++ Hospital/Hospital/Class_Map/Drugs/DrugType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hospital.Class_Map.Drugs
+{
+
+    // will be tha base class of the "drug" thingymaging
+    public class DrugType
+    {
+    }
+}
---
+++ Hospital/Hospital/Class_Map/Humans/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hospital
+{
+    public abstract class Person
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string  Adress { get; set; }
+        public string  PhoneNR { get; set; }
+       public Person(string FName,string LName,string Adress,string PhoneNR)
+        {
+
+        }
+
+    }
+}

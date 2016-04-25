2576ff6edfef0fc23941b06210f5b5e2ffb2d40d
lundgren84 <lundgren84@hotmail.se>
2016-04-25 14:38:51 +0200

    Staff and patients!!

--- Hospital/Hospital/Class_Map/Humans/Staff.cs
+++ Hospital/Hospital/Class_Map/Humans/Staff.cs
@@ -4,17 +4,20 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Hospital.Class_Map.Humans;
+using Hospital.Class_Map.RoomDepartment;
+using Hospital.Class_Map.Treatment;
 
 namespace Hospital
 {
-    public class Staff 
+    public class Staff
     {
-        // P-Key
+        // ---------P-Key------------
         public int StaffID { get; set; }
 
-        // Properties
+        // ---------Properties--------
         [Required]
-        [StringLength(50)]
+        [StringLength(50, ErrorMessage = " You cant have longer name then 50 chars!!")]
         [Display(Name = "First Name")]
         public string FName { get; set; }
         [Required]
@@ -26,23 +29,22 @@ namespace Hospital
         public string PhoneNR { get; set; }
         [Display(Name = "Social Security Number")]
         [StringLength(10)]
-        public string SSN { get; set; }
-        [DataType(DataType.Date)]
-        public DateTime CeckInHospital { get; set; }
+        public string SSN { get; set; }       
         [DataType(DataType.Date)]
         public DateTime HierDate { get; set; }
         [Display(Name = "Full Name")]
         public string Name
         { get { return FName + " " + LName; } }
 
-        // F-Keys
-        public int ProfessionID { get; set; }
-        public int DepartmentID { get; set; }
+        // ----------F-Keys------------
+        public Proffesion ProffesionID { get; set; }
+        public Department DepartmentID { get; set; }
+        public ICollection<Treatment> Treatments { get; set; }
 
-
-        //public Staff(string FName, string LName, string Adress, string PhoneNr):base(FName,LName,Adress,PhoneNr)
-        //{
-
-        //}
+        //-----------Construktor-------
+        public Staff()
+        {
+            this.Treatments = new HashSet<Treatment>();
+        }
     }
 }

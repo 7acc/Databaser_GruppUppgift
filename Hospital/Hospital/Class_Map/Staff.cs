9568bac0c948f656b23926876170d9c15aaaf8b7
lundgren84 <lundgren84@hotmail.se>
2016-04-25 11:33:09 +0200

    Remove inheranse

--- Hospital/Hospital/Class_Map/Staff.cs
+++ Hospital/Hospital/Class_Map/Staff.cs
@@ -5,13 +5,17 @@ using System.Web;
 
 namespace Hospital
 {
-    public class Staff : Person
+    public class Staff 
     {
         public int StaffID { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Adress { get; set; }
+        public string PhoneNR { get; set; }
 
-        public Staff(string FName, string LName, string Adress, string PhoneNr):base(FName,LName,Adress,PhoneNr)
-        {
+        //public Staff(string FName, string LName, string Adress, string PhoneNr):base(FName,LName,Adress,PhoneNr)
+        //{
 
-        }
+        //}
     }
 }

2d4d85c16085811399589c5b487db0b53fb1f702
henrikbernhardsson <henke.ber@gmail.com>
2016-04-26 09:33:32 +0200

    Fixed causes

--- THIS_Hospital/THIS_Hospital/Class_Map/Cause/Cause.cs
+++ THIS_Hospital/THIS_Hospital/Class_Map/Cause/Cause.cs
@@ -7,8 +7,16 @@ namespace Hospital.Class_Map.Cause
 {
     public class Cause
     {
+        //-------P-Key------
         public int CauseID { get; set; }
+
+        //-------Name of the cause-------
+        [Required]
+        [StringLength(50, ErrorMessage = " You can't have longer cause then 50 chars!!")]
+        [Display(Name = "Cause")]
         public string CauseName { get; set; }
-        public int CauseTypeID { get; set; }
+
+        //-------F-Key------
+        public CauseType CauseTypeID { get; set; }
     }
 }
--- THIS_Hospital/THIS_Hospital/Class_Map/Cause/CauseType.cs
+++ THIS_Hospital/THIS_Hospital/Class_Map/Cause/CauseType.cs
@@ -7,7 +7,13 @@ namespace Hospital.Class_Map.Cause
 {
     public class CauseType
     {
+        //-------P-Key-------
         public int CauseTypeID { get; set; }
+
+        //-------Cause-Type Name-------
+        [Required]
+        [StringLength(50, ErrorMessage = " You can't have longer cause-type then 50 chars!!")]
+        [Display(Name = "Cause Type")]
         public string CauseTypeName { get; set; }
     }
 }

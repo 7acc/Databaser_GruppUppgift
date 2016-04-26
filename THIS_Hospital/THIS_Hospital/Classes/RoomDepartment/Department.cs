ead4850b51d58ff4569432dfe8f7b6319532c912
7acc <jacc321@gmail.com>
2016-04-26 09:33:35 +0200

    "jahopp" se omd e funkar nu då!?!?

--- THIS_Hospital/THIS_Hospital/Classes/RoomDepartment/Department.cs
+++ THIS_Hospital/THIS_Hospital/Classes/RoomDepartment/Department.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace THIS_Hospital
 {
     public class Department
     {
+        //------------------------------------------------  
+        public int DepartentID { get; set; }
+
+        //------------------------------------------------  
+
+        [StringLength(20)]
+        public string DepartmentName { get; set; }
+
+        
     }
 }
--- THIS_Hospital/THIS_Hospital/Classes/RoomDepartment/Room.cs
+++ THIS_Hospital/THIS_Hospital/Classes/RoomDepartment/Room.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace THIS_Hospital
 {
     public class Room
     {
+        //------------------------------------------------  
+        public int RoomID { get; set; }
+
+        //------------------------------------------------  
+        [MaxLength(3, ErrorMessage = "Cant be longer than 3 digits")]
+        public int RoomNr { get; set; }
+
+        //------------------------------------------------  
+        public bool Avalible { get; set; }
+
+        //------------------------------------------------  
+        public virtual Department department { get; set; }
+
     }
 }

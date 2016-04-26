a4e7d6ca2cd78143aed544b7284db4a0610b2f7a
7acc <jacc321@gmail.com>
2016-04-26 10:42:01 +0200

    added a text file for questions to the DB

--- THIS_Hospital/THIS_Hospital/Classes/RoomDepartment/Room.cs
+++ THIS_Hospital/THIS_Hospital/Classes/RoomDepartment/Room.cs
@@ -10,16 +10,20 @@ namespace THIS_Hospital
     public class Room
     {
         //------------------------------------------------  
+        [Required]
         public int RoomID { get; set; }
 
         //------------------------------------------------  
+        [Required]
         [MaxLength(3, ErrorMessage = "Cant be longer than 3 digits")]
         public int RoomNr { get; set; }
 
-        //------------------------------------------------  
+        //------------------------------------------------ 
+        [Required]
         public bool Avalible { get; set; }
 
         //------------------------------------------------  
+        [Required]
         public virtual Department department { get; set; }
 
     }
